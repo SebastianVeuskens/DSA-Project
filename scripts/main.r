e0d2dbaa8ac00956fd3dc6f01da3b0aec099b0c4
Sebastian Veuskens <sebastian.veuskens@googlemail.com>
2023-05-03 14:42:32 +0200

    Load Data set

--- scripts/main.r
+++ scripts/main.r
@@ -1,1 +1,4 @@
 # Max Schlake, Jose Gabriel Escarraman, Desmond Reynolds, Sebastian Veuskens
+setwd(file_path <- dirname(rstudioapi::getSourceEditorContext()$path))
+
+heart_data <- read.csv("../data/S1Data.csv")
